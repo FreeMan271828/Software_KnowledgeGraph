766a32f89138b0496f91e97868cfc152dc522e0c
FreeMan <1114509193@qq.com>
2024-05-31 23:53:01 +0800

    hsy

---
+++ src/main/java/com/freeman/software_knowledge_graph/SoftwareKnowledgeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.freeman.software_knowledge_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareKnowledgeGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareKnowledgeGraphApplication.class, args);
+	}
+
+}
